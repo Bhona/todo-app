5fa457a61dd0f81522c8f4fbd55670c95a8de66e
Bhona <bayron.barahona@gmail.com>
2020-08-10 22:38:27 -0600

    updates

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'ung50g30f4'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'mh16w66flh'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/devel`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'bhona.auth0.com', // Auth0 domain
   clientId: 'AhgaFb7YwmLaT7bmqvAVhhJGx42m1g0B', // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://134.122.20.151:3000/callback'
 }
